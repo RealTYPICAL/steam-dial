2e8857ef655bc9f919c7bdd1590a2eed70e97031
jetter019@msn.com <jetter019@msn.com>
2017-12-05 00:30:53 +0100

    Hello world. Now start working on making this a dial service.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 
+
+import * as peer from "peer-dial";
+import * as express from 'express';
+import * as steam from 'steam-url-api';
+import * as http from 'http';
+import * as cp from "child_process";
+import * as something from 'ts-npm-module';
+
 console.log('hello world');
